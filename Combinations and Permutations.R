8bbd6f0921d73861e74b1ebee5198723010cb996
Sowat <sowat.hossain.rafi@g.bracu.ac.bd>
2020-06-16 16:19:07 +0600

    Add some key points in Combinations and Permutations.

--- Combinations and Permutations.R
+++ Combinations and Permutations.R
@@ -1,3 +1,11 @@
+# Key points
+
+# paste() joins two strings and inserts a space in between.
+# expand.grid() gives the combinations of 2 vectors or lists.
+# permutations(n,r) from the gtools package lists the different ways that r items can be selected from a set of n options when order matters.
+# combinations(n,r) from the gtools package lists the different ways that r items can be selected from a set of n options when order does not matter.
+
+
 # Code : Introducing paste() and expand.grid()
 # joining strings with paste
 number <- "Three"
